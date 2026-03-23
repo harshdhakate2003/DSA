5d327ead238d1389876e62aff25c257db13e00a7
Harsh Dhakate <harshdhakate7517@gmail.com>
2026-03-23 16:41:51 +0530

    Day1 Practice

---
+++ day1-Problems/script.js
@@ -0,0 +1,103 @@
+//-------------Write a  program to find power of a number using for loop.
+
+// let num = 5
+// let pow = 5
+// let ans = 1
+
+// for(let i=1;i<=pow;i++)
+// {
+//     ans=ans*num
+// }
+// console.log(ans)
+
+
+//--------------Write a program to find all factors of a number.
+
+// let num=121
+
+// for(let i=1;i<=Math.floor(num/2);i++)
+// {
+//     if(num%i==0)
+//     {
+//         console.log(i)
+//     }
+// }
+
+
+//--------------Write a program to calculate factorial of a number.
+
+// let num=5
+// let fact=1
+
+// while(num>=2)
+// {
+//     fact=fact*num
+//     num--
+// }
+// console.log(fact)
+
+
+//---------------Write a program to find HCF (GCD) of two numbers.
+
+
+// let a=198465
+// let b=48915
+
+// while(a!=b)
+// {
+//     if(a>b)
+//     {
+//         a=a-b
+//     }else{
+//         b=b-a
+//     }
+// }
+// console.log(a)
+
+
+//---------------Write a program to find LCM of two numbers.
+
+
+// let num1=12
+// let i=1
+// let ans1=num1
+
+// let num2=14
+// let j=1
+// let ans2=num2
+
+// while(ans1!=ans2)
+// {
+//     if(ans1<ans2)
+//     {
+//         i++
+//         ans1=num1*i
+//     }else{
+//         j++
+//         ans2=num2*j
+//     }
+// }
+
+// console.log(ans1)
+
+
+
+//--------------More Questions-------------------
+
+// write a program to check whether a number is Prime number or not.
+
+// Write a program to print all Prime numbers between 1 to n.
+
+// Write a program to find sum of all prime numbers between 1 to n.
+
+// Write a program to find all prime factors of a number.
+
+// Write a program to check whether a number is Armstrong number or not.
+
+// Write a program to print all Armstrong numbers between 1 to n.
+
+// Write a program to check whether a number is Perfect number or not.
+
+// Write a program to print all Perfect numbers between 1 to n.
+
+
