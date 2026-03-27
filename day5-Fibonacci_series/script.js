bbe0ad7a844eec01f29cab1fd4a0b6ab9a4a0928
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-03-27 09:04:57 +0530

    Day5 Practice

---
+++ day5-Fibonacci_series/script.js
@@ -0,0 +1,105 @@
+// Write a  program to print Fibonacci series up to n terms.
+
+let count=10
+let n1=0
+let n2=1
+console.log(n1)
+console.log(n2)
+for(let i=1;i<=count-2;i++)
+{
+    n3=n1+n2
+    console.log(n3)
+
+    n1=n2
+    n2=n3
+}
+
+
+
+//-------------------------Questions---------------------------------
+
+// Write a  program to print all natural numbers from 1 to n. – using while loop/for
+                                                                 
+
+// Write a  program to print all natural numbers in reverse (from n to 1). – using while loop
+
+// Write a  program to print all alphabets from a to z. – using while loop
+
+
+// Write a  program to print all even numbers between 1 to 100. – using while loop
+
+// Write a  program to print all odd number between 1 to 100.
+
+// Write a  program to find sum of all natural numbers between 1 to n.
+
+// Write a  program to find sum of all even numbers between 1 to n.
+
+// Write a  program to find sum of all odd numbers between 1 to n.
+
+// Write a  program to print multiplication table of any number.
+
+// =============================================================================
+
+// Write a  program to count number of digits in a number.
+
+
+
+
+// Write a  program to find first and last digit of a number.
+// Write a  program to find sum of first and last digit of a number.
+
+// Write a  program to swap first and last digits of a number. using arr
+
+
+// Write a  program to calculate sum of digits of a number.
+
+// Write a  program to calculate product of digits of a number.
+
+// Write a  program to enter a number and print its reverse.
+
+// Write a  program to check whether a number is palindrome or not.
+
+// Write a  program to find frequency of each digit in a given integer.
+
+
+
+
+// Write a  program to enter a number and print it in words.
+
+
+
+// Write a  program to print all ASCII character with their values.
+
+
+
+
+
+// Write a  program to find power of a number using for loop.
+
+// Write a  program to find all factors of a number.
+
+// Write a  program to calculate factorial of a number.
+
+// Write a  program to find HCF (GCD) of two numbers.
+
+// Write a  program to find LCM of two numbers.
+
+// Write a  program to check whether a number is Prime number or not.
+
+// Write a  program to print all Prime numbers between 1 to n.
+
+// Write a  program to find sum of all prime numbers between 1 to n.
+
+// Write a  program to find all prime factors of a number.
+
+// Write a  program to check whether a number is Armstrong number or not.
+
+// Write a  program to print all Armstrong numbers between 1 to n.
+
+// Write a  program to check whether a number is Perfect number or not.
+
+// Write a  program to print all Perfect numbers between 1 to n.
+
+// Write a  program to check whether a number is Strong number or not.
+// Write a  program to print all Strong numbers between 1 to n.
+// Write a  program to print Fibonacci series up to n terms.
