1114de5b0226246d365bcddbd4f852bcbd605007
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-03-28 09:11:57 +0530

    Day6 Practice

---
+++ day6-Array_1/script.js
@@ -0,0 +1,26 @@
+// Array -> array are variable or data structure that store the data in the consecutive memory location,
+//          index of an array always starts from zero
+
+
+//          0  1  2  3  4  5    6      7  
+// let arr=[11,12,13,14,15,16,"Harsh",true]
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+
+// for(let i=0;i<=arr.length-1;i++)
+// {
+//     console.log(arr[i])
+// }
+
+// ---------------------------------------------------------
+
+
+let arr2=["Harsh","Ayush","Mahesh","Nitish"]
+
+console.log(arr2)
+arr2[4]="Gaurav"
+arr2[2]="Aditya"
+arr2[6]="Mayur"
+console.log(arr2)
+
