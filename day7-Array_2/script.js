5e7e3c4a9cf2d008a58108f6143046a8e74c8d75
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-03-29 16:18:08 +0530

    Day7 practice

---
+++ day7-Array_2/script.js
@@ -0,0 +1,86 @@
+// arrays->are the vcarible or datastructure that stores the data in consecutive memory location , index of arrays always starts from zero
+// length-> it return the count total el of an arr
+
+// let arr=[]
+
+// arr[0]="apple"
+// arr[1]="mango"
+
+// arr[3]="kiwi"
+// console.log(arr)
+
+
+//-------------------------------------------------------------------
+// to insert element into array
+// push()->it is used to insert the element from end of an arr
+//unshift()-> it is used to insert the element from begning of an arr
+
+// let aa=arr.push("apple")
+// let bb=arr.push("mango")
+// console.log(arr)
+// arr.push("watermelon")
+// let k=arr.unshift("kiwi")
+// arr.unshift("banana1")
+// arr.unshift("banana2")
+// arr.unshift("banana3")
+// console.log(arr)
+
+
+
+//-------------------------------------------------------------------
+// to remove the element from arr
+// pop()-> it will remove the element from end of an arr
+// shift()-> it will remove the element from start of  arr
+
+// let abc=arr.pop()
+// arr.pop()
+// arr.pop()
+// arr.pop()
+// arr.shift()
+// arr.shift()
+// arr.shift()
+// console.log(arr.shift())
+
+// console.log(arr)
+// console.log(abc)
+// console.log(aa)
+// console.log(bb)
+// console.log(k)
+
+
+//-------------------------------------------------------------------
+//-------------------Methos of Array--------------------------------
+
+// let arr1=["Harsh","Ayush","Mahesh","Raj","Yash"]
+// let arr2=["Aditya","Pratik","Mahesh","Harsh"]
+
+// let arr3=arr1.concat(arr2)
+
+// console.log(arr1)
+// console.log(arr2)
+// console.log(arr3)
+
+
+// console.log(arr3.at(3))
+// console.log(arr3.indexOf("Mahesh"))
+// console.log(arr3.lastIndexOf("Mahesh"))
+// console.log(arr3.indexOf("Ayush"))
+// console.log(arr3.indexOf("Rushi"))
+
+// console.log(arr3.includes("Ayush"))
+// console.log(arr3.includes("Mukesh"))
+
+
+//-------------------------------------------------------------------
+
+// let  animals = ["ant", "bison", "camel", "duck", "elephant","tiger","lion","zebra","cat","dog"];
+
+// console.log(animals)
+// let rev=animals.toReversed()
+// console.log(rev)
+
+
+// let numbers=[1,2,3,4,5,6]
+// console.log(numbers.toString())
+// console.log(animals.toString())
+
