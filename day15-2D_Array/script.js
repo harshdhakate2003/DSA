c80f71df62a70b2067bbc5b4a70752f86de574bc
harshdhakate2003 <harshdhakate7517@gmail.com>
2026-04-07 09:02:45 +0530

    Day15 practice

---
+++ day15-2D_Array/script.js
@@ -0,0 +1,68 @@
+// 1-dimension array
+
+let arr1=[5,6,4,7,8,9,5,1]
+
+// 2-dimension array
+//              0             1            2
+let arr2=[["a","b","c"],["d","e","f"],["g","h","i"]] 
+//          0   1   2     0   1   2     0   1   2
+
+
+// 3-dimensin array
+
+let arr3=[[["a","b"],["c","d"]],[["e","f"],["g","h"]]]
+
+//-------------------------------------------------------------------------------------
+//----------------2 Dimension Array-------------------------------
+
+
+// let arr=[
+//      ["a","b","c"],
+//      ["d","e","f"],
+//      ["g","h","i"]
+//     ]
+
+
+// console.log(arr[0][0])
+// console.log(arr[0][1])
+// console.log(arr[0][2])
+
+// console.log(arr[1][0])
+// console.log(arr[1][1])
+// console.log(arr[1][2])
+
+// console.log(arr[2][0])
+// console.log(arr[2][1])
+// console.log(arr[2][2])
+
+//----------------------------------------------------------------
+
+
+// let arr=[
+//      ["a","b","c","p","@"],
+//      ["d","e","f","q","#","-"],
+//      ["g","h","i","r","$","+","*"],
+//      ["s","t","u","v","%","&"]
+//     ]
+
+
+   
+// for(let i=0;i<=arr.length-1;i++)
+// {
+//     for(let j=0;j<=arr[i].length-1;j++)
+//     {
+//         console.log(arr[i][j])
+//     }
+// }
+
+
+// for(let i=0;i<=arr.length-1;i++)
+// {
+//     for(let j=arr[i].length-1;j>=0;j--)
+//     {
+//         console.log(arr[i][j])
+//     }
+// }
+
+
+ 
